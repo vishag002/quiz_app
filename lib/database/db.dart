af5cc10bfde670e11198cd9b0f7a1988e4d46079
vishag002 <vvishag477@gmail.com>
2024-05-22 16:07:21 +0530

    data base

---
+++ lib/database/db.dart
@@ -0,0 +1,25 @@
+class DataBase {
+  static List<Map> myDB = [
+    {
+      "question": "which prgraming Language is used to build Flutter apps ",
+      "options": ["Java", "Python", "Dart", "C"],
+      "answer": 2,
+    },
+    {
+      "question":
+          "Who developed the Flutter Framework and continues to maintain it today?",
+      "options": ["FaceBook ", "Microsoft", "Oracle", "Google"],
+      "answer": 3,
+    },
+    {
+      "question": "How many types of widgets are there in Flutter? ",
+      "options": ["2", "4", "6", "8+"],
+      "answer": 0,
+    },
+    {
+      "question": "A sequence of asynchronous Flutter events is known as a:?",
+      "options": ["Flow ", "Current", "Stream", "Series"],
+      "answer": 2,
+    }
+  ];
+}
