c1bd7e233b6a04622c731469e6206f9ce5983300
vishag002 <vvishag477@gmail.com>
2024-06-07 08:33:53 +0530

    ui update

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/database/db.dart';
+import 'package:quiz_app/utilis/color_const.dart';
+import 'package:quiz_app/view/result_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,19 +16,31 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    startCountDown();
   }
 
   int i = 0;
   int count = DataBase.myDB.length;
-
   int? checkValue;
+  bool showAnswer = false;
+  int score = 0;
+
+  void showAns(int index) {
+    int ansIndex = DataBase.myDB[i]['answer'];
+    if (index == ansIndex) {
+      showAnswer = true;
+    }
+    checkValue = index;
+    setState(() {});
+  }
 
   //check function
   void check(int index) {
     int ansIndex = DataBase.myDB[i]['answer'];
-
+    if (checkValue == null && index == ansIndex) {
+      score++;
+    }
     checkValue = index;
     setState(() {});
   }
@@ -33,10 +50,25 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       if (i <= count - 2) {
         i++;
+        checkValue = null;
+        startCountDown();
+      } else {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ResultScreen(
+                score: score,
+              ),
+            ));
       }
     });
     checkValue = null;
-    print(count);
+    showAnswer = false;
+    setState(() {
+      startCountDown();
+      timerValue = 15;
+    });
+    //print(count);
   }
 
   //prev button
@@ -48,184 +80,232 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  //timer
+  int timerValue = 15;
+  Timer? countdownTimer;
+  void startCountDown() {
+    int ansIndex = DataBase.myDB[i]['answer'];
+    countdownTimer?.cancel();
+    countdownTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (timerValue > 0) {
+          timerValue--;
+        } else {
+          timer.cancel();
+          showAns(ansIndex);
+          // showAnswer = true;
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    double h1 = MediaQuery.of(context).size.height;
+    double w1 = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-              height: 400,
-              width: double.infinity,
-              //    color: Colors.amber[100],
-              child: Stack(
-                children: [
-                  Container(
-                    height: 300,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.lightBlue[900],
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40),
-                        bottomLeft: Radius.circular(50),
-                        bottomRight: Radius.circular(50),
+      backgroundColor: ColorConst.background,
+      body: Container(
+        height: h1,
+        width: w1,
+        decoration: BoxDecoration(
+          gradient: ColorConst.backgroundG,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                height: 400,
+                width: double.infinity,
+                //    color: Colors.amber[100],
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 300,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: Colors.lightBlue[900],
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(40),
+                            topRight: Radius.circular(40),
+                            bottomLeft: Radius.circular(50),
+                            bottomRight: Radius.circular(50),
+                          ),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://as1.ftcdn.net/v2/jpg/04/67/10/02/1000_F_467100228_IB9BaE1WU8pHRgDruKi2WhcHpMQtrTYK.jpg"),
+                              fit: BoxFit.fitHeight,
+                              scale: 1)),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 25,
+                      child: Center(
+                        child: Container(
+                          height: 200,
+                          width: 340,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.lightBlue.withOpacity(0.1),
+                                spreadRadius: 5,
+                                blurRadius: 10,
+                                blurStyle: BlurStyle.solid,
+                                offset: Offset(3, 3),
+                              )
+                            ],
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.white,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Question ${i + 1}/25",
+                                style: TextStyle(
+                                    color: ColorConst.textColor,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              SizedBox(height: 30),
+                              Text(
+                                DataBase.myDB[i]['question'],
+                                style: TextStyle(
+                                    decoration: TextDecoration.underline,
+                                    color: ColorConst.textColor,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500),
+                                textAlign: TextAlign.center,
+                              )
+                            ],
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    left: 25,
-                    child: Center(
-                      child: Container(
-                        height: 200,
-                        width: 340,
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.lightBlue.withOpacity(0.1),
-                              spreadRadius: 5,
-                              blurRadius: 10,
-                              blurStyle: BlurStyle.solid,
-                              offset: Offset(3, 3),
-                            )
-                          ],
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.white,
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Question ${i + 1}/25",
-                              style: TextStyle(
-                                  color: Colors.lightBlue[900],
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                            SizedBox(height: 30),
-                            Text(
-                              DataBase.myDB[i]['question'],
-                              style: TextStyle(
-                                  decoration: TextDecoration.underline,
-                                  color: Colors.lightBlue[900],
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500),
-                              textAlign: TextAlign.center,
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    child: Center(
-                      child: CircleAvatar(
-                        radius: 45,
+                    Positioned(
+                      child: Center(
                         child: CircleAvatar(
-                          radius: 40,
-                          backgroundColor: Colors.lightBlue[900],
+                          radius: 45,
+                          child: CircleAvatar(
+                            radius: 40,
+                            child: Text(
+                              timerValue.toString(),
+                              style: GoogleFonts.aBeeZee(
+                                color: ColorConst.textColor,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            backgroundColor: ColorConst.secondary,
+                          ),
+                          backgroundColor: Colors.white,
                         ),
-                        backgroundColor: Colors.white,
                       ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 4,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        check(index);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 65,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            color: checkValue == index
+                                ? checkValue == DataBase.myDB[i]['answer']
+                                    ? Colors.lightGreen
+                                    : Colors.deepOrange.shade800
+                                : Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(70),
+                                bottomRight: Radius.circular(70)),
+                            /* border: Border.all(
+                              color: Colors.lightBlue.shade900.withOpacity(0.9),
+                              width: 5,
+                              style: BorderStyle.solid,
+                            ), */
+                          ),
+                          child: Center(
+                            child: Text(
+                              DataBase.myDB[i]['options'][index],
+                              style: TextStyle(
+                                color: ColorConst.background,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: prev,
+                    child: Container(
+                      height: 50,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.amber.shade900,
+                      ),
+                      child: Center(
+                          child: Text(
+                        "prev",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500),
+                      )),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: next,
+                    child: Container(
+                      height: 50,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.amber.shade900,
+                      ),
+                      child: Center(
+                          child: Text(
+                        "next",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500),
+                      )),
                     ),
                   ),
                 ],
               ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50),
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                  return InkWell(
-                    onTap: () {
-                      check(index);
-                    },
-                    child: Container(
-                      height: 80,
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: checkValue == index
-                            ? checkValue == DataBase.myDB[i]['answer']
-                                ? Colors.lightGreen
-                                : Colors.deepOrange.shade800
-                            : Colors.transparent,
-                        borderRadius: BorderRadius.circular(30),
-                        border: Border.all(
-                          color: Colors.lightBlue.shade900.withOpacity(0.9),
-                          width: 5,
-                          style: BorderStyle.solid,
-                        ),
-                      ),
-                      child: Center(
-                        child: Text(
-                          DataBase.myDB[i]['options'][index],
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  onTap: prev,
-                  child: Container(
-                    height: 50,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.blue.shade900.withOpacity(.9),
-                    ),
-                    child: Center(
-                        child: Text(
-                      "prev",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500),
-                    )),
-                  ),
-                ),
-                InkWell(
-                  onTap: next,
-                  child: Container(
-                    height: 50,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.blue.shade900.withOpacity(.9),
-                    ),
-                    child: Center(
-                        child: Text(
-                      "next",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500),
-                    )),
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
