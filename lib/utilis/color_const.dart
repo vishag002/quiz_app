fdb448d0ec190db6b0430642f6f7e352d07e50f5
vishag002 <vvishag477@gmail.com>
2024-06-07 08:33:42 +0530

    color_const

--- lib/utilis/color_const.dart
+++ lib/utilis/color_const.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
 class ColorConst {
+  static Color textColor = Colors.grey.shade900;
   static Color primary = Colors.white;
-  static Color secondary = Colors.white;
-  static Color background = Colors.white;
+  static Color secondary = Colors.yellow.shade600;
+  static Color background = Colors.black;
+  static const Gradient backgroundG = SweepGradient(
+      colors: [
+        Colors.yellow,
+        Colors.yellow,
+        Colors.yellow,
+        Colors.yellow,
+        Colors.yellow,
+        Colors.orange,
+        Colors.yellow,
+        Colors.yellow,
+        Colors.yellow,
+      ],
+      endAngle: 15,
+      center: Alignment.topCenter,
+      tileMode: TileMode.repeated,
+      transform: GradientRotation(5),
+      startAngle: 5);
 }
